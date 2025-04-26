0dad1b2ebf3542843b55c677f6c2671cba34f9da
Kelly Lukito <klukito@usc.edu>
2025-04-26 06:34:46 +0000

    done hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,19 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool canScheduleWorker(const AvailabilityMatrix& avail,
+                       const size_t maxShifts,
+                       DailySchedule& sched,
+                       size_t day,
+                       Worker_T worker);
+
+bool scheduleHelper(const AvailabilityMatrix& avail,
+                    const size_t dailyNeed,
+                    const size_t maxShifts,
+                    DailySchedule& sched,
+                    vector<size_t>& shiftsPerWorker,
+                    size_t day,
+                    size_t slot);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -37,9 +50,62 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    sched.resize(avail.size(), vector<Worker_T>(dailyNeed, INVALID_ID));
+    vector<size_t> shiftsPerWorker(avail[0].size(), 0);
 
-
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsPerWorker, 0, 0);
 
 
 }
 
+//recursive helper for backtracking
+bool scheduleHelper(const AvailabilityMatrix& avail,
+                    const size_t dailyNeed,
+                    const size_t maxShifts,
+                    DailySchedule& sched,
+                    vector<size_t>& shiftsPerWorker,
+                    size_t day,
+                    size_t slot)
+{
+  const size_t numDays = avail.size();
+  const size_t numWorkers = avail[0].size();
+
+  //base case: all days/slots filled
+  if(day >= numDays){
+    return true;
+  }
+  //all shifts for day filled->move to next day
+  if(slot >= dailyNeed){
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsPerWorker, day + 1, 0);
+  }
+  //try to schedule all workerss
+  for(Worker_T worker = 0; worker < numWorkers; worker++){
+    //check if current worker has available shifts
+    if(avail[day][worker] && shiftsPerWorker[worker] < maxShifts){
+      //check if current worker is already shifted for current day
+      bool alreadySched = false;
+      for (size_t i = 0; i < slot; i++){
+        if(sched[day][i] == worker){
+          alreadySched = true;
+          break;
+        }
+      }
+      //if not already scheduled for that day, assign that slot to worker
+      if(!alreadySched){
+        sched[day][slot] = worker;
+        shiftsPerWorker[worker]++;
+        //recursively try to fill all spots
+        if(scheduleHelper(avail, dailyNeed, maxShifts, sched, shiftsPerWorker, day, slot + 1)){
+          return true;
+        }
+        //if it doesn't work set back to normal
+        sched[day][slot] = INVALID_ID;
+        shiftsPerWorker[worker]--;
+      }
+    }
+  }
+  //if it gets here, one spot didn't work and then you backtrack
+  return false;
+
+
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelper(string& current, const string& in, string remaining_float, const set<string>& dict, set<string>& result, size_t pos);
+
 
 
 // Definition of primary wordle function
@@ -22,7 +24,67 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> result;
+    string curr = in;
+    wordleHelper(curr, in, floating, dict, result, 0);
+    return result;
+
 
 }
 
 // Define any helper functions here
+void wordleHelper(string& current, const string& in, string remaining_float, const set<string>& dict, set<string>& result, size_t pos){
+    //base case. All positions full
+    if(pos == in.length()){
+        //make sure it is a word
+        if(dict.find(current) != dict.end()){
+            //make sure all floating character used
+            if(remaining_float.empty()){
+                result.insert(current);
+            }
+        }
+        return;
+    }
+
+    //position already has filled in letter
+    if(in[pos] != '-'){
+        //if it is in remaining_float remove it
+        if(remaining_float.find(in[pos])<remaining_float.length()){
+            remaining_float.erase(remaining_float.find(in[pos]), 1);
+        }
+        wordleHelper(current, in, remaining_float, dict, result, pos+1);
+    }
+    //if position is not yet filled
+    else{
+        for(size_t i=0; i<remaining_float.length(); ++i){
+            current[pos] = remaining_float[i];
+
+            string new_remaining = remaining_float;
+            new_remaining = new_remaining.erase(i,1);
+            
+            wordleHelper(current, in, new_remaining, dict, result, pos+1);
+        }
+
+        int remaining_slots = in.length()-pos-1;
+        int fixed_slots = 0;
+
+        for(size_t i=pos+1; i<in.length(); ++i){
+            if(in[i] != '-'){
+                fixed_slots++;
+            }
+        }
+        int open_slots = remaining_slots - fixed_slots;
+
+        //ensure remaining floating chars are used first
+        if(open_slots >= int(remaining_float.length())){
+            for(char c= 'a'; c <= 'z'; ++c){
+                //skip if remaining char is there
+                if(remaining_float.find(c) < remaining_float.length()){
+                    continue;
+                }
+                current[pos] = c;
+                wordleHelper(current, in, remaining_float, dict, result, pos + 1);
+            }
+        }
+    }
+}
